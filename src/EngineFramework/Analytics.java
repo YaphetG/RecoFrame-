686c1050097f214be108e30c3b99402d927566b2
YaphetG <yaphet.gobenak@gmail.com>
2017-06-15 10:26:09 -0500

    Round two commit

--- src/EngineFramework/Analytics.java
+++ src/EngineFramework/Analytics.java
@@ -12,13 +12,7 @@ public enum Analytics {
 	public void SetUserCollection(ModelCollection userCollection){
 		this.userCollection = userCollection;
 	}
-	public void runOnProduct(ARecommenderAlgorithm recommender){
-		//pass product data
-	}
-	public void runOnUser(ARecommenderAlgorithm recommender){
-		//pass user data
-	}
-	public void runAsCorelation(ARecommenderAlgorithm recommender){
-		//pass both user and product data
+	public void runProductCollection(ARecommenderAlgorithm reco){
+		reco.runOnDataSet(productCollection);
 	}
 }
